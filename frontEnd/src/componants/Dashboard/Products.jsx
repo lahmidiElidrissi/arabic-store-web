213c14ae7b85dd403d462bb633df1642abf380fb
lahmidiElidrissi <elidrissilahmidi@gmail.com>
2024-09-01 14:41:24 +0100

    add loader to products and categories pages in dashboard

--- frontEnd/src/componants/Dashboard/Products.jsx
+++ frontEnd/src/componants/Dashboard/Products.jsx
@@ -4,6 +4,7 @@ import axiosHttpClient from '../../Utils/api';
 import { useNavigate } from 'react-router-dom';
 import Toastify from 'toastify-js'
 import { customStylesDatatable, paginationOptionsWithArabicLang } from '../../Utils/helpers';
+import Loader from '../loader';
 
 const MainContent = () => {
 
@@ -42,10 +43,10 @@ const MainContent = () => {
   };
 
   const columns = [
-    { name: 'الصورة', selector: (row) => <img src={row?.images[0]?.path} alt={row?.name} className="w-[150px] h-[150px]" style={{ objectFit : 'cover'}}></img> },
+    { name: 'الصورة', selector: (row) => <img src={row?.images[0]?.path} alt={row?.name} className="w-[90px] h-[90px] md:w-[150px] md:h-[150px]" style={{ objectFit: 'cover' }}></img> },
     { name: 'اسم', selector: (row) => row?.name, sortable: true },
     { name: 'وصف', selector: (row) => row?.description ? row.description.slice(0, 50) + "..." : "", sortable: true },
-    { name: 'السعر', selector: (row) => row?.price, sortable: true },
+    { name: 'السعر', width : '100px' , selector: (row) => row?.price, sortable: true },
   ];
 
   // function to delete product
@@ -53,7 +54,7 @@ const MainContent = () => {
     axiosHttpClient.delete(`${import.meta.env.VITE_URL_BACKEND}/remove/product/${id}`)
       .then(response => {
         fetchData();
-        if ( response.status === 200 ) {
+        if (response.status === 200) {
           Toastify({
             text: "تم الحذف بنجاح",
             className: "text-1xl",
@@ -64,7 +65,7 @@ const MainContent = () => {
               width: "70%",
             },
           }).showToast();
-        }else{
+        } else {
           Toastify({
             text: "ليس لديك صلاحية الحذف",
             className: "text-1xl",
@@ -90,18 +91,18 @@ const MainContent = () => {
   const enhancedColumns = [
     ...columns,
     {
-      name: 'Actions',
+      name: '',
       cell: (row) => (
         <>
           <button
             onClick={() => onUpdate(row.id)}
-            className="bg-yellow-500 m-1 px-4 py-1 rounded text-white hover:bg-yellow-600 text-sm"
+            className="bg-yellow-500 m-1 px-4 py-1 m-2 rounded text-white hover:bg-yellow-600 text-sm"
           >
             تعديل
           </button>
           <button
             onClick={() => onDelete(row.id)}
-            className="bg-red-500 m-1 px-4 py-1 rounded text-white hover:bg-red-700 text-sm"
+            className="bg-red-500 m-1 px-4 py-1 m-2 rounded text-white hover:bg-red-700 text-sm"
           >
             حذف
           </button>
@@ -110,30 +111,34 @@ const MainContent = () => {
       ignoreRowClick: true,
       allowOverflow: true,
       button: true,
-      width: '150px',
+      width: '200px',
     },
   ];
 
   return (
     <div className="flex-1 py-5 md:p-6 md:pr-64 bg-slate-100">
-      <div className='mx-5'>
-        <h2 class="text-3xl font-bold !leading-tight text-black sm:text-4xl md:text-[45px] md:p-5 text-center"> المنتجات</h2>
-        <input
-          className='placeholder:italic placeholder:text-slate-400 block bg-white w-auto border border-slate-300 rounded-md py-1 pr-2 shadow-sm focus:outline-none focus:border-yellow-100 focus:ring-yellow-600 focus:ring-1 sm:text-sm m-5'
-          type="text"
-          placeholder="  البحث عن منتج"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <DataTable
-          columns={enhancedColumns}
-          data={data}
-          pagination
-          onSort={handleSort}
-          customStyles={customStylesDatatable}
-          paginationComponentOptions={paginationOptionsWithArabicLang}
-        />
-      </div>
+      {
+        data.length > 0 ?
+          <div className='mx-5'>
+            <h2 class="text-3xl font-bold !leading-tight text-black sm:text-4xl md:text-[45px] md:p-5 text-center"> المنتجات</h2>
+            <input
+              className='placeholder:italic placeholder:text-slate-400 block bg-white w-auto md:w-3/12 border border-slate-300 rounded-md py-1 pr-2 shadow-sm focus:outline-none focus:border-yellow-100 focus:ring-yellow-600 focus:ring-1 sm:text-sm m-5'
+              type="text"
+              placeholder="  البحث عن منتج"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <DataTable
+              columns={enhancedColumns}
+              data={data}
+              pagination
+              onSort={handleSort}
+              customStyles={customStylesDatatable}
+              paginationComponentOptions={paginationOptionsWithArabicLang}
+            />
+          </div>
+          : <Loader />
+      }
     </div>
   );
 };
